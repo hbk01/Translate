f20002f80e00d955367611b44272e5a4c3eff501
hbk01 <3243430237@qq.com>
2020-12-13 18:57:06 +0800

    + 小修改

--- app/src/main/java/cn/hbkcn/translate/databases/DatabaseHelper.kt
+++ app/src/main/java/cn/hbkcn/translate/databases/DatabaseHelper.kt
@@ -1,6 +1,5 @@
 package cn.hbkcn.translate.databases
 
-import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -8,17 +7,17 @@ import android.database.sqlite.SQLiteOpenHelper
 
 const val version = 1
 const val name = "Translate"
-const val table_history = "history"
+val tables = HashMap<String, String>()
 
 class DatabaseHelper(c: Context) : SQLiteOpenHelper(c, name, null, version) {
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql = """
-            CREATE TABLE IF NOT EXISTS $table_history (
+        tables["TranslateHistory"] = """
+            CREATE TABLE IF NOT EXISTS TranslateHistory (
                 time INTEGER PRIMARY KEY NOT NULL,
                 json TEXT NOT NULL
             );
         """
-        db?.execSQL(sql)
+        tables.onEach { entry -> db?.execSQL(entry.value) }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -31,7 +30,7 @@ class DatabaseHelper(c: Context) : SQLiteOpenHelper(c, name, null, version) {
             put("time", System.currentTimeMillis())
             this
         }
-        writableDatabase.insert(table_history, "", values)
+        writableDatabase.insert(tables["TranslateHistory"], "", values)
     }
 
     /**
@@ -39,25 +38,26 @@ class DatabaseHelper(c: Context) : SQLiteOpenHelper(c, name, null, version) {
      * @param limit default is 10, it's set result limit.
      * @return HashMap<Int, String> Int is insert time, String is insert json
      */
-    @SuppressLint("Recycle")
     fun selectHistory(limit: Int = 10): LinkedHashMap<Long, String> {
         val map = LinkedHashMap<Long, String>()
-        val query = "SELECT * FROM $table_history ORDER BY time DESC LIMIT $limit;"
+        val query = "SELECT * FROM TranslateHistory ORDER BY time DESC LIMIT $limit;"
         val cursor = readableDatabase.rawQuery(query, emptyArray())
         while (cursor.moveToNext()) {
             val time = cursor.getLong(cursor.getColumnIndex("time"))
             val json = cursor.getString(cursor.getColumnIndex("json"))
             map[time] = json
         }
+        cursor.close()
         return map
     }
 
-    @SuppressLint("Recycle")
     fun selectLastHistory(): String {
-        val sql = "SELECT * FROM $table_history ORDER BY time DESC;"
+        val sql = "SELECT * FROM TranslateHistory ORDER BY time DESC;"
         val cursor = readableDatabase.rawQuery(sql, emptyArray())
         cursor.moveToFirst()
-        return cursor.getString(cursor.getColumnIndex("json"))
+        val json = cursor.getString(cursor.getColumnIndex("json"))
+        cursor.close()
+        return json
     }
 }
 
